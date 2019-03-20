c4015d989c8b17b6376e964eb1c90803b764f7b2
muk <muk@abc.com>
2019-03-20 15:13:16 +0700

    Back Process Scess

--- AuthenSSRU/AuthenSNRU/AuthenSNRU/RegisterViewController.swift
+++ AuthenSSRU/AuthenSNRU/AuthenSNRU/RegisterViewController.swift
@@ -14,17 +14,15 @@ class RegisterViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    } //Main Method
+    
+
+    @IBAction func BeckButton(_ sender: UIBarButtonItem) {
+        
+        print("You Click Back")
+        performSegue(withIdentifier: "BackMain", sender: self)
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
+} //Main Class
