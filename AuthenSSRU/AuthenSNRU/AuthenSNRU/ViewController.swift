6cebeb715eda95e559ec41624b9b7b510a761d9c
muk <muk@abc.com>
2019-03-20 09:59:48 +0700

    wallpaperAndlogo

---
+++ AuthenSSRU/AuthenSNRU/AuthenSNRU/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AuthenSNRU
+//
+//  Created by iMacStudent25 on 19/3/2562 BE.
+//  Copyright © 2562 Tiwaporn. All rights reserved.
+//
+
+
+
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
