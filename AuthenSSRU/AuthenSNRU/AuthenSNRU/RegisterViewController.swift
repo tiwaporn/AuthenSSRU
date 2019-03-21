5e0cf95527ad75771b1d1780792ab61efa027908
muk <muk@abc.com>
2019-03-21 16:19:57 +0700

    register Success

--- AuthenSSRU/AuthenSNRU/AuthenSNRU/RegisterViewController.swift
+++ AuthenSSRU/AuthenSNRU/AuthenSNRU/RegisterViewController.swift
@@ -42,7 +42,7 @@ class RegisterViewController: UIViewController {
         if (name.count == 0) || (user.count == 0) || (password.count == 0) {
             myAlert(titleString: "Have Sppsce", messageString: "Please Fill All Blank")
         } else {
-            
+            uploadData(name: name, user: user, password: password)
         }
         
         
@@ -64,8 +64,29 @@ class RegisterViewController: UIViewController {
     } //myAlert
     
     func uploadData(name:String,user:String,password:String) -> Void {
+       
         let urlString = "https://www.androidthai.in.th/snru/addDatamuk.php?isAdd=true&Name=\(name)&User=\(user)&Password=\(password)"
+        let url = URL(string: urlString)
+        let request = NSMutableURLRequest(url: url!)
+        let task = URLSession.shared.dataTask(with: request as URLRequest){data,response,error in
+            
+            if error != nil {
+                print(error)
+            } else {
+                
+                if let testData = data {
+                    
+                let canReadData = NSString(data: testData, encoding: String.Encoding.utf8.rawValue)
+                    
+                    print("canRead ==> \(String(describing: canReadData))")
+                    
+                 }
+                
+            } //if1
+            
+        }// end Task
         
+        task.resume()
         
         
     }
